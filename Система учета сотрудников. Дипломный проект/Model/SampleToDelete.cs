f5d5465a9e46167b1bd66c36c2e5c10c42f4faa5
Shed2005 <dvikashe@gmail.com>
2024-09-19 21:32:44 +1000

    какой-то бред, че-то стерла, какую-то заготовку мелкую сделала

---
+++ Система учета сотрудников. Дипломный проект/Model/SampleToDelete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Система_учета_сотрудников._Дипломный_проект.Model
+{
+    public class SampleToDelete
+    {
+        public string TextList { get; set; }
+    }
+}
