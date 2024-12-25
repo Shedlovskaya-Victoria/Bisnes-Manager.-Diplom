3f9b84d84d59c0b4bdff1c365ec12e0c874a6dee
Shed2005 <dvikashe@gmail.com>
2024-12-25 16:49:00 +1000

    add DI to RequestApp and DatabasePersistens for control exemplars injection database and middleware. Injections cultivation out work class for more lite change work class and not stretch configuration code to all app

---
+++ BisnesManager.DatabaseApp/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using BisnesManager.DatabasePersistens.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BisnesManager.DatabasePersistens
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDBPersistens(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<BissnesExpertSystemDiplomaContext>(options =>
+            {
+                options.UseNpgsql(connectionString);
+            });
+            services.AddScoped<BissnesExpertSystemDiplomaContext>(provider => provider.GetService<BissnesExpertSystemDiplomaContext>());
+            return services;
+        }
+    }
+}
---
+++ BisnesManager.RequestsApp/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using BisnesManager.DatabasePersistens.Model;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BisnesManager.RequestsApp
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddRequestApp(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
